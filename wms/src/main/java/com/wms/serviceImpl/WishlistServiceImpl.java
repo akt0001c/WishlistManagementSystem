0aebfea84fce095e14020bc0a9dfce815016104a
akt0001c <110126989+akt0001c@users.noreply.github.com>
2024-02-19 10:46:42 +0530

    wishlist end point work done

--- wms/src/main/java/com/wms/serviceImpl/WishlistServiceImpl.java
+++ wms/src/main/java/com/wms/serviceImpl/WishlistServiceImpl.java
@@ -1,8 +1,16 @@
 package com.wms.serviceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.wms.entity.Product;
+import com.wms.entity.User;
 import com.wms.entity.Wishlist;
+import com.wms.entity.WishlistDetails;
+import com.wms.exceptions.ProductNotFoundException;
+import com.wms.exceptions.SomethingwentWrongException;
+import com.wms.exceptions.UserNotLoggedException;
 import com.wms.repository.ProductRepository;
 import com.wms.repository.UserRepository;
 import com.wms.repository.WishlistRepository;
@@ -31,20 +39,47 @@ public class WishlistServiceImpl implements WishlistService {
 
 	@Override
 	public String addItem(Integer pid, String email) {
+		User user= urepo.findByEmail(email).orElseThrow(()->new UserNotLoggedException("User shouble be logged in "));
+		Wishlist wishlist= wrepo.findByUser(user.getUserId()).orElseThrow(()->new SomethingwentWrongException("wishlist not found for the logged user"));
+		Product product = prepo.findById(pid).orElseThrow(()->new ProductNotFoundException("Product not found for id :"+pid));
+		wishlist.setUser(user);
+		WishlistDetails wd= new WishlistDetails();
+		wd.setProduct(product);
+		wd.setWishlist(wishlist);
+		product.getWishlistDetails().add(wd);
+		wishlist.getWishlistDetails().add(wd);
 		
-		return null;
+		
+	    wrepo.save(wishlist);
+		
+		return "Item added in wishlist sucessfully";
 	}
 
 	@Override
 	public Wishlist getAllWishListItem(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		User user= urepo.findByEmail(email).orElseThrow(()->new UserNotLoggedException("User shouble be logged in "));
+		Wishlist wishlist= wrepo.findByUser(user.getUserId()).orElseThrow(()->new SomethingwentWrongException("wishlist not found for the logged user"));
+		return wishlist;
 	}
 
 	@Override
 	public String removeItem(Integer pid, String email) {
-		// TODO Auto-generated method stub
-		return null;
+		User user= urepo.findByEmail(email).orElseThrow(()->new UserNotLoggedException("User shouble be logged in "));
+		Wishlist wishlist= wrepo.findByUser(user.getUserId()).orElseThrow(()->new SomethingwentWrongException("wishlist not found for the logged user"));
+		List<WishlistDetails> wlist= wishlist.getWishlistDetails();
+		for(WishlistDetails ob :wlist)
+		{
+			if(ob.getProduct().getPid()==pid)
+			{
+				wlist.remove(ob);
+				break;
+			}
+		}
+		
+		wishlist.setWishlistDetails(wlist);
+		
+		wrepo.save(wishlist);
+		return "Item has been removed from the wishlist";
 	}
 
 }
